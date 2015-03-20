dbc5708209f49b7bee38c8ea1aae4a83984f9858
yngsva <yngve.svalestuen@ciber.com>
2015-03-20 09:32:11 +0100

    gh-deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,33 @@
 var gulp = require('gulp'),
     rename = require('gulp-rename'),
     mainBowerFiles = require('main-bower-files'),
-    inject = require('gulp-inject');
+    inject = require('gulp-inject'),
+    ghPages = require('gh-pages'),
+    path = require('path'),
+    del = require('del');
 
-gulp.task('index', function () {
+gulp.task('clean', function (cb) {
+    del(['dist'], cb);
+});
+    
+gulp.task('js', ['clean'], function () {
+    return gulp.src(mainBowerFiles()).pipe(gulp.dest('./dist'));
+});
+
+gulp.task('index', ['clean', 'js'], function () {
     return gulp.src('src/app.html')
         .pipe(rename('index.html'))
-        .pipe(inject(gulp.src(mainBowerFiles(), {read: false})))
-        .pipe(gulp.dest('./'));
+        .pipe(inject(gulp.src(['./dist/**.js', './dist/**.css']), {ignorePath: '/dist'}))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch-index', function () {
     gulp.watch('src/app.html', ['index']);
+});
+
+gulp.task('build', ['clean', 'js', 'index']);
+gulp.task('default', ['build']);
+
+gulp.task('deploy', ['build'], function (cb) {
+    ghPages.publish(path.join(process.cwd(), 'dist'), cb);
 });
