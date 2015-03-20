4d082ba2554a0e7c307cb13af65e76bf14a744df
yngsva <yngve.svalestuen@ciber.com>
2015-03-20 10:06:47 +0100

    not add root slash

--- gulpfile.js
+++ gulpfile.js
@@ -10,14 +10,17 @@ gulp.task('clean', function (cb) {
     del(['dist'], cb);
 });
     
-gulp.task('js', ['clean'], function () {
+gulp.task('bower-files', ['clean'], function () {
     return gulp.src(mainBowerFiles()).pipe(gulp.dest('./dist'));
 });
 
-gulp.task('index', ['clean', 'js'], function () {
+gulp.task('index', ['clean', 'bower-files'], function () {
     return gulp.src('src/app.html')
         .pipe(rename('index.html'))
-        .pipe(inject(gulp.src(['./dist/**.js', './dist/**.css']), {ignorePath: '/dist'}))
+        .pipe(inject(gulp.src(['./dist/**/*.js', './dist/**/*.css'], {read: false}), {
+            ignorePath: 'dist', 
+            addRootSlash: false
+        }))
         .pipe(gulp.dest('./dist'));
 });
 
@@ -25,7 +28,7 @@ gulp.task('watch-index', function () {
     gulp.watch('src/app.html', ['index']);
 });
 
-gulp.task('build', ['clean', 'js', 'index']);
+gulp.task('build', ['clean', 'bower-files', 'index']);
 gulp.task('default', ['build']);
 
 gulp.task('deploy', ['build'], function (cb) {
